e56f8187e0839a9aeedbbaea107ebdfddb058e55
emekBaris <baris.kucuktabak@gmail.com>
2020-11-06 21:08:39 -0600

    complete structure with publisher subscriber and dynamic reconfigure

--- include/multi_robot_interaction/MultiRobotInteraction.h
+++ include/multi_robot_interaction/MultiRobotInteraction.h
@@ -12,25 +12,36 @@
 #ifndef SRC_LEGSCONTROLLER_H
 #define SRC_LEGSCONTROLLER_H
 
+// fundamentals
 #include <ros/ros.h>
 #include <Eigen/Core>
 #include <chrono>
 #include <math.h>
 
+// msg types
 #include <std_srvs/SetBool.h>
-#include <geometry_msgs/Wrench.h>
 #include <sensor_msgs/JointState.h>
+#include <std_msgs/Float64MultiArray.h>
+
+// dynamic reconfigure
+#include <dynamic_reconfigure/server.h>
+#include <multi_robot_interaction/dynamic_paramsConfig.h>
 
 class MultiRobotInteraction {
 public:
-    MultiRobotInteraction(ros::NodeHandle& nodeHandle);
+    MultiRobotInteraction(ros::NodeHandle &nodeHandle);
+
     void initialize();
     void advance();
     void exit();
-
 private:
 
-    ros::NodeHandle& nodeHandle_;
+    ros::NodeHandle &nodeHandle_;
+
+    bool startInteractionFlag_;
+    int interaction_mode_;
+    double k_interaction_;
+    double c_interaction_;
 
     //vector of subscribers to subscribe joint states of the robots
     std::vector<ros::Subscriber> jointStateSubscribers_;
@@ -39,15 +50,19 @@ private:
     std::vector<ros::Publisher> jointCommandPublishers_;
     std::vector<sensor_msgs::JointState> jointCommandMsgs_;
     std::vector<ros::Publisher> interactionEffortCommandPublishers_; // effort = either force or torque
-    std::vector<geometry_msgs::Wrench> interactionEffortCommandMsgs_;
+    std::vector<std_msgs::Float64MultiArray> interactionEffortCommandMsgs_;
 
-    void jointStateCallback(const sensor_msgs::JointStateConstPtr & msg, int robot_id);
+    // joint state subscriber callback
+    void jointStateCallback(const sensor_msgs::JointStateConstPtr &msg, int robot_id);
+
+    // dynamic reconfigure server and callback
+    dynamic_reconfigure::Server<multi_robot_interaction::dynamic_paramsConfig> server_;
+    void dynReconfCallback(multi_robot_interaction::dynamic_paramsConfig &config, uint32_t level);
 
     // service server to start or stop the interaction
     ros::ServiceServer startInteractionService_;
-    bool startExoServiceCallback(std_srvs::SetBool::Request& req,
-                                 std_srvs::SetBool::Response& res);
-    bool startInteractionFlag_;
+    bool startExoServiceCallback(std_srvs::SetBool::Request &req,
+                                 std_srvs::SetBool::Response &res);
 
     // vector of namespaces that holds the namespace of the robots in concern
     std::vector<std::string> nameSpaces_;
@@ -58,7 +73,14 @@ private:
     Eigen::MatrixXd jointVelocityMatrix_;
     Eigen::MatrixXd jointTorqueMatrix_;
 
-};
+    Eigen::MatrixXd jointPositionCommandMatrix_; // each column corresponds to different robots
+    Eigen::MatrixXd jointVelocityCommandMatrix_;
+    Eigen::MatrixXd jointTorqueCommandMatrix_;
 
+    Eigen::MatrixXd interactionEffortCommandMatrix_;
+
+    void publishJointCommands();
+    void publishInteractionEffortCommand();
+};
 
 #endif //SRC_LEGSCONTROLLER_H
--- src/MultiRobotInteraction.cpp
+++ src/MultiRobotInteraction.cpp
@@ -19,10 +19,11 @@ void MultiRobotInteraction::initialize() {
     // sizing vectors
     jointStateSubscribers_ = std::vector<ros::Subscriber>(numberOfRobots_);
     jointCommandPublishers_ = std::vector<ros::Publisher>(numberOfRobots_);
+    interactionEffortCommandPublishers_ = std::vector<ros::Publisher>(numberOfRobots_);
     jointCommandMsgs_ = std::vector<sensor_msgs::JointState>(numberOfRobots_);
-    interactionEffortCommandMsgs_ = std::vector<geometry_msgs::Wrench>(numberOfRobots_);
+    interactionEffortCommandMsgs_ = std::vector<std_msgs::Float64MultiArray>(numberOfRobots_);
 
-    // defines the subscribers and publishers
+    // define the subscribers, publishers
     for(int i = 0; i< numberOfRobots_; i++){
         std::string jointStateSubscribersTopicName = "/" + nameSpaces_[i] + "/joint_states";
         jointStateSubscribers_[i] = nodeHandle_.subscribe<sensor_msgs::JointState>(jointStateSubscribersTopicName, 1,
@@ -33,24 +34,48 @@ void MultiRobotInteraction::initialize() {
                 (jointCommandPublishersTopicName, 1);
 
         std::string interactionEffortCommandPublishersTopicName = "/" + nameSpaces_[i] + "/interaction_effort_commands";
-        interactionEffortCommandPublishers_[i] = nodeHandle_.advertise<geometry_msgs::Wrench>
+        interactionEffortCommandPublishers_[i] = nodeHandle_.advertise<std_msgs::Float64MultiArray>
                 (interactionEffortCommandPublishersTopicName, 1);
     }
+
+    // set dynamic parameter server
+    dynamic_reconfigure::Server<multi_robot_interaction::dynamic_paramsConfig>::CallbackType f;
+    f = boost::bind(&MultiRobotInteraction::dynReconfCallback, this, _1, _2);
+    server_.setCallback(f);
 
     startInteractionService_ = nodeHandle_.advertiseService("start_interaction", &MultiRobotInteraction::startExoServiceCallback, this);
     startInteractionFlag_ = false;
 
+    // initializing joint and interaction matrixes
     jointPositionMatrix_ = Eigen::MatrixXd::Zero(robotsDoF_, numberOfRobots_);
     jointVelocityMatrix_ = Eigen::MatrixXd::Zero(robotsDoF_, numberOfRobots_);
     jointTorqueMatrix_ = Eigen::MatrixXd::Zero(robotsDoF_, numberOfRobots_);
-
+    jointPositionCommandMatrix_ = Eigen::MatrixXd::Zero(robotsDoF_, numberOfRobots_);
+    jointVelocityCommandMatrix_ = Eigen::MatrixXd::Zero(robotsDoF_, numberOfRobots_);
+    jointTorqueCommandMatrix_ = Eigen::MatrixXd::Zero(robotsDoF_, numberOfRobots_);
+    interactionEffortCommandMatrix_ = Eigen::MatrixXd::Zero(robotsDoF_, numberOfRobots_);
 }
 
 void MultiRobotInteraction::advance() {
     if(!startInteractionFlag_) return;
 
+    if(interaction_mode_ == 1){ // robot 2 mimics robot 1's position
+        for(int dof = 0; dof<robotsDoF_; dof++){
+            jointPositionCommandMatrix_(dof, 1) = jointPositionMatrix_(dof, 0);
+        }
+        publishJointCommands();
+    }
+    else if(interaction_mode_ == 2){ // virtual haptic spring
 
+        for(int dof = 0; dof<robotsDoF_; dof++){
+            interactionEffortCommandMatrix_(dof, 0) =
+                    k_interaction_*(jointPositionMatrix_(dof, 1) - jointPositionMatrix_(dof, 0));
+            interactionEffortCommandMatrix_(dof, 1) =
+                    k_interaction_*(jointPositionMatrix_(dof, 0) - jointPositionMatrix_(dof, 1));
+        }
 
+        publishInteractionEffortCommand();
+    }
 }
 
 void MultiRobotInteraction::exit() {
@@ -81,4 +106,38 @@ bool MultiRobotInteraction::startExoServiceCallback(std_srvs::SetBool::Request &
 
     res.success = true;
     return true;
+}
+
+void MultiRobotInteraction::dynReconfCallback(multi_robot_interaction::dynamic_paramsConfig &config, uint32_t level) {
+
+    interaction_mode_ = config.interaction_mode;
+    k_interaction_ = config.k_interaction;
+    c_interaction_ = config.c_interaction;
+    return;
+}
+
+void MultiRobotInteraction::publishJointCommands() {
+
+    for(int robot = 0; robot<numberOfRobots_; robot++){
+        jointCommandMsgs_[robot].position.resize(robotsDoF_);
+        jointCommandMsgs_[robot].velocity.resize(robotsDoF_);
+        jointCommandMsgs_[robot].effort.resize(robotsDoF_);
+        for(int dof = 0; dof<robotsDoF_; dof++){
+            jointCommandMsgs_[robot].position[dof] = jointPositionCommandMatrix_(dof, robot);
+            jointCommandMsgs_[robot].velocity[dof] = jointVelocityCommandMatrix_(dof, robot);
+            jointCommandMsgs_[robot].effort[dof] = jointTorqueCommandMatrix_(dof, robot);
+        }
+        jointCommandPublishers_[robot].publish(jointCommandMsgs_[robot]);
+    }
+}
+
+void MultiRobotInteraction::publishInteractionEffortCommand() {
+
+    for(int robot = 0; robot<numberOfRobots_; robot++){
+        interactionEffortCommandMsgs_[robot].data.resize(robotsDoF_);
+        for(int dof = 0; dof<robotsDoF_; dof++){
+            interactionEffortCommandMsgs_[robot].data[dof] = interactionEffortCommandMatrix_(dof, robot);
+        }
+        interactionEffortCommandPublishers_[robot].publish(interactionEffortCommandMsgs_[robot]);
+    }
 }
